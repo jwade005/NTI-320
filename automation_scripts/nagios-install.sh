9c512be4ac29da8147a76d5966c664983c856eec
Jonathan Wade <jwade005@seattlecentral.edu>
2017-04-09 06:38:24 -0700

    add nrpe support

--- automation_scripts/nagios-install.sh
+++ automation_scripts/nagios-install.sh
@@ -3,7 +3,14 @@
 #nagios core server install from source
 #***** run as root *****
 
-yum install -y wget httpd php gcc glibc glibc-common gd gd-devel make net-snmp unzip
+#clone jwade005's github
+yum -y install git
+git clone https://github.com/jwade005/NTI-320.git /tmp/NTI-320
+
+
+yum install -y wget httpd php gcc glibc glibc-common gd gd-devel make net-snmp unzip openssl-devel
+
+setenforce 0
 
 cd /tmp
 
@@ -62,13 +69,83 @@ chkconfig --add httpd
 chkconfig --level 35 httpd on
 
 #run pre-flight check on configuration data
-/usr/sbin/nagios -v /etc/nagios/nagios.cfg
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
+
+#install nrpe-3
+cd ~
+wget https://github.com/NagiosEnterprises/nrpe/archive/3.0.1.tar.gz
+
+#extract nrpe-3
+tar xvf 3.0.1.tar.gz
+cd nrpe-3.0.1
+
+#configure nrpe-3
+./configure --enable-command-args --with-nagios-user=nagios --with-nagios-group=nagios --with-ssl=/usr/bin/openssl --with-ssl-lib=/usr/lib/x86_64-linux-gnu
+
+#build and install NRPE
+make all
+make install
+
+#add firewall rules for nrpe communication over tcp:5666
+firewall-cmd --zone=public --add-port=5666/tcp
+firewall-cmd --zone=public --add-port=5666/tcp --permanent
+
+#edit nrpe.cfg file
+sed -i '/^allowed_hosts=/s/$/,10.138.0.0\/24/' /usr/local/nagios/etc/nrpe.cfg
+sed -i 's/^dont_blame_nrpe=.*/dont_blame_nrpe=1/g' /usr/local/nagios/etc/nrpe.cfg
+
+#add support for nrpe and other commands in commands.cfg
+echo "define command{
+  command_name check_nrpe
+  command_line $USER1$/check_nrpe -H $HOSTADDRESS$ -c $ARG1$
+  }
+
+# 'check_load' comand defintion
+define command{
+	command_name	check_load
+	command_line $USER1$/check_load -w $ARG1$ -c $ARG2$
+	}
+
+# 'check_disk' command definition
+define command{
+  command_name    check_disk
+  command_line    $USER1$/check_disk -w $ARG1$ -c $ARG2$ -p $ARG3$
+  }
+
+# 'check_procs' command definition
+define command{
+  command_name    check_procs
+  command_line    $USER1$/check_procs -w $ARG1$ -c $ARG2$ -s $ARG3$
+  }
+
+# 'check_users' command definition
+define command{
+  command_name    check_users
+  command_line    $USER1$/check_users -w $ARG1$ -c $ARG2$
+  }" >> /usr/local/nagios/etc/objects/commands.cfg
+
+#open server directory for monitoring configurations
+sed -i "s,#cfg_dir=\/usr\/local\/nagios\/etc\/servers,cfg_dir=\/usr\/local\/nagios\/etc\/servers,g" /usr/local/nagios/etc/nagios.cfg
+
+#make the directory
+mkdir /usr/local/nagios/etc/servers
+
+#add configuration file for server to directory
+cp /tmp/NTI-320/config_files/nagios/test-1-nti320.cfg /usr/local/nagios/etc/servers/test-1-nti320.cfg
+
+#perform config check
+/usr/local/nagios/bin/nagios -v /usr/local/nagios/etc/nagios.cfg
+
+#restart services
+systemctl restart nagios
+systemctl restart httpd
 
 #login to the nagios web interface at http://<your.nagios.server.ip>/nagios; u/n: nagiosadmin and password created earlier
 
 
 #add local timezone support
-#vim nagios.cfg   ----> #timezone offste ----> add line: use_timezone=US/Pacific
+#vim nagios.cfg   ----> #timezone offset ----> add line: use_timezone=US/Pacific
 #vim /etc/httpd/conf.d/nagios.conf ---->will change timezone in CGI ---->add line: SetEnv TZ "US/Pacific" under <options execCGI>
 #systemctl restart nagios
 #systemctl restart httpd
