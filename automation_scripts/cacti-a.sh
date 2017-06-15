de1185db191a5af43775051626e22f5794175e6b
Jonathan Wade <jwade005@seattlecentral.edu>
2017-06-14 18:28:09 -0700

    final stuff

--- automation_scripts/cacti-a.sh
+++ automation_scripts/cacti-a.sh
@@ -76,6 +76,8 @@ mycactiserver="cacti-a"                      # set this to your cacti server
 myreposerver="yum-repo"                       # set this to your repo server
 mynagiosserverip="35.185.217.151"                   # set this to the ip address of your nagios server
 
+
+sudo chmod +x /tmp/NTI-320/automation_scripts/generate_config.sh
 ./tmp/NTI-320/automation_scripts/generate_config.sh $1 $2              # code I gave you in a previous assignment that generates a nagios config
 
 gcloud compute copy-files $1.cfg $myusername@$mynagiosserver:/etc/nagios/conf.d
