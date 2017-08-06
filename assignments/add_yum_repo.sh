e473c68790a0bf439a0c8470d22c635133991c5c
Jonathan Wade <jwade005@seattlecentral.edu>
2017-08-06 15:23:19 -0700

    Create add_yum_repo.sh

---
+++ assignments/add_yum_repo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# adds yum repo on a server
+
+
+ip=$(gcloud compute instances list | grep yumrepo-a | awk '{print $4}') # will dynamically add ip address
+echo "[jwade005repo]
+
+name=jwade005repo NTI-320 - $basearch
+
+baseurl=http://$ip/7/OS/x86_64/Packages/
+
+enabled=1
+
+gpgcheck=0
+
+" >> /etc/yum.repos.d/NTI-320.repo
+                                                                    # Now that the repo is added, list all repos and make sure
+                                                                    # it shows up without error
+yum repolist
+
+isokay=`yum search "hello world" | grep "Matched: hello world"`
+
+if [ -z "$isokay" ]; then
+   echo "There's somthing wrong with your repo... check yum repolist to see if it shows up then try installing a package"
+else
+   echo "All is well and we found your test package: $isokay"
+fi
