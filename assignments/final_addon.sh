3948a77b52671242e0fb931875433aa9f4ec424f
Jonathan Wade <jwade005@seattlecentral.edu>
2017-12-30 10:54:06 -0600

    Add-on to final code to monitor instances w/ Nagios

---
+++ assignments/final_addon.sh
@@ -0,0 +1,26 @@
+myusername="Jonathan"                         # set this to your username
+mynagiosserver="nagios-a"                     # set this to your nagios server name
+mycactiserver="cacti-a"                      # set this to your cacti server
+myreposerver="yumrepo-a"                       # set this to your repo server
+mynagiosserverip="35.197.81.237"                   # set this to the ip address of your nagios server
+
+generate_config.sh $1 $2              # code I gave you in a previous assignment that generates a nagios config
+
+gcloud compute copy-files $1.cfg $myusername@$mynagiosserver:/etc/nagios/conf.d
+
+                                      # note: I had to add user my gcloud user to group nagios using usermod -a -G nagios $myusername on my nagios server in order to make this work.
+                                      # I also had to chmod 770 /etc/nagios/conf.d
+
+
+gcloud compute ssh $myusername@$mynagiosserver \
+"sudo /usr/sbin/nagios -v /etc/nagios/nagios.cfg" \
+| grep "Things look okay - No serious problems" \
+
+
+if [[ $configstatus ]]; then
+   gcloud compute ssh $myusername@$mynagiosserver "sudo systemctl restart nagios"
+   echo "$1 has been added to nagios."
+else
+   echo "There was a problem with the nagios config, please log into $mynagiosserver and run /usr/sbin/nagios -v /etc/nagios/nagios.cfg to figure out where the problem is";
+   exit 1;
+fi
