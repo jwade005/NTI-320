15df683b51ff7d4a7308253d761c61fa9155af38
Jonathan Wade <jwade005@seattlecentral.edu>
2017-12-30 12:09:10 -0600

    Create rpm_buid_server.sh
    Build a RPM Build Server

---
+++ assignments/rpm_buid_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# centos 7 rpm build server install script
+
+# Install the RPM-Build package and the macros and helper scripts package:
+sudo yum -y install rpm-build
+sudo yum -y install redhat-rpm-config
+
+# To create the RPM building environment, run the two commands below:
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+echo '%_topdir %(echo $HOME)/rpmbuild' > ~/.rpmmacros
+
+# Install extra packages with some useful tools and stuff:
+sudo yum -y install make
+sudo yum -y install yum-utils # This Package includes utilities that make yum easier and more powerful to use!
+
+# Edit the following file to enable the centos source repos
+#sudo vim /etc/yum.repos.d/CentOS-Sources.repo and enable the repos for access to source packages
+
+sudo sed -i 's/enabled=0/enabled=1/g' /etc/yum.repos.d/CentosOS-Sources.repo
