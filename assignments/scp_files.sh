7a070d6a36c712cc77bff5271aea49e5fbc91ea8
Jonathan Wade <jwade005@seattlecentral.edu>
2018-02-24 09:32:54 -0800

    Create scp_files.sh
    Update to "Write a bash or python script that lets you SCP files to all your servers" assignment.

---
+++ assignments/scp_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in $( gcloud compute instances list --zones us-west1-a | awk '{print $1}' | grep -v "NAME" );\
+do gcloud compute ssh --zone us-west1-a Jonathan@$i --command "gcloud scp <filename>";\
+done;
